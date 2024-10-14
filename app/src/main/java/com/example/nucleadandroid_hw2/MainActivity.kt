d31e8822492c26beef2fe97b8bf7cc27538b64d7
Maxim Kovtun <frostohelp@ya.ru>
2024-10-14 17:30:24 +0300

    обновил цвет фона, добавил кнопки-заглушки

--- app/src/main/java/com/example/nucleadandroid_hw2/MainActivity.kt
+++ app/src/main/java/com/example/nucleadandroid_hw2/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.nucleadandroid_hw2
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonToActivityB = findViewById<Button>(R.id.button_to_activity_B)
+
+        buttonToActivityB.setOnClickListener {
+
         }
+
+        val buttonToFragmentB = findViewById<Button>(R.id.button_to_activity_B)
+
+        buttonToFragmentB.setOnClickListener {
+
+        }
+
     }
 }
