c2a293377b231c97dc3e8e8a5ba5b60fa08d005c
Maxim Kovtun <frostohelp@ya.ru>
2024-10-14 18:26:22 +0300

    2. Исправил опечатку с двумя ссылками на кнопку ActivityB; добавил переход на ActivityB по кнопке

--- app/src/main/java/com/example/nucleadandroid_hw2/MainActivity.kt
+++ app/src/main/java/com/example/nucleadandroid_hw2/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.nucleadandroid_hw2
 
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +17,12 @@ class MainActivity : AppCompatActivity() {
         val buttonToActivityB = findViewById<Button>(R.id.button_to_activity_B)
 
         buttonToActivityB.setOnClickListener {
-
+            val intent = Intent(this, ActivityB::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+            startActivity(intent)
         }
 
-        val buttonToFragmentB = findViewById<Button>(R.id.button_to_activity_B)
+        val buttonToFragmentB = findViewById<Button>(R.id.button_to_fragment_B)
 
         buttonToFragmentB.setOnClickListener {
 
